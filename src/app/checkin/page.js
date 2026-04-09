89a4ad8221909eba8f3d776854186d3c5a40a4d5
Noman <noman99_@hotmail.com>
2026-04-09 08:08:02 +0200

    feat: add checkin and tips pages

---
+++ src/app/checkin/page.js
@@ -0,0 +1,7 @@
+export default function CheckIn() {
+  return (
+    <main>
+      <h1>Check-in</h1>
+    </main>
+  );
+}
---
+++ src/app/tips/page.js
@@ -0,0 +1,7 @@
+export default function Tips() {
+  return (
+    <main>
+      <h1>Tips</h1>
+    </main>
+  );
+}
