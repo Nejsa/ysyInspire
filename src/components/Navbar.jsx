9cdf0b6e411eddb89e95bf71e196b87edde176f0
Noman <noman99_@hotmail.com>
2026-04-09 08:23:48 +0200

    feat: add Navbar component to layout

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable @next/next/no-html-link-for-pages */
+export default function Navbar() {
+  return (
+    <nav>
+      <p>ysyInspire</p>
+      <ul>
+        <li>
+          <a href="/">Hjem</a>
+        </li>
+        <li>
+          <a href="checkin">Check-in</a>
+        </li>
+        <li>
+          <a href="tips">Tips</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
